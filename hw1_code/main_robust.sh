9efa41f267b2200796c386222927ebd646789a96
chen-zichen <ciciechen@gmail.com>
2022-02-15 05:57:58 +0800

    add robust command

---
+++ hw1_code/main_robust.sh
@@ -0,0 +1,16 @@
+# robust model version
+# 2. fgsm + untargeted ce
+python3 evaluate.py --model_name pgd10_eps8.pth --fgsm
+
+# 3. pgd + untargeted ce + t=10
+python3 evaluate.py --model_name pgd10_eps8.pth --attack_step 10
+
+# 4. pgd + untargeted cw + t=10
+python3 evaluate.py --model_name pgd10_eps8.pth --attack_step 10 --loss_type cw
+
+# 5. pgd + targeted cw + t=10
+python3 evaluate.py --model_name pgd10_eps8.pth --attack_step 1 --loss_type cw --targeted
+
+# 6. pgd + ce + different settings... example
+python3 evaluate.py --model_name pgd10_eps8.pth --attack_step 20 --loss_type ce 
+python3 evaluate.py --model_name pgd10_eps8.pth --attack_step 10 --loss_type ce --eps 1
